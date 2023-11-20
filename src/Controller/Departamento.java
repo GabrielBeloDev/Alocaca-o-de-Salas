603c7097a296a86c8416d92ec82b97030d58d7ad
GabrielBeloDev <gabrielbelodev@outlook.com>
2023-11-20 12:22:44 -0300

    adding method name and parameters

--- src/Controller/Departamento.java
+++ src/Controller/Departamento.java
@@ -12,4 +12,20 @@ public class Departamento {
     public Departamento(Hashtable<Espaco, ArrayList<Solicitacao>> alocados) {
         this.alocados = alocados;
     }
+
+    public void adicionarSolitacao(Solicitacao solicitacao, String tipo_solicitacao){
+        //TODO
+    }
+
+    public boolean solicitacaoCasual(String tipo){
+        //TODO
+    }
+
+    public boolean checar(String tipo){
+        //TODO
+    }
+
+    public void verificarCompatibilidade(ArrayList<Espaco> espacos){
+        //TODO
+    }
 }
